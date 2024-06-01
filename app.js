d59a81d3234ec73b54e8171df6bedce199fb83d3
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-06-02 00:44:49 +0500

    done

--- app.js
+++ app.js
@@ -29,9 +29,17 @@ function colorPalettes() {
     "#B8FF33", // Lime
     "#FF33FF", // Fuchsia
     "#33FFF0", // Electric Blue
+    "#FFD733", // Mustard
+    "#33FFD7",  // Turquoise
+    "#0F9033", // Tangerine
+    "#33FF99",  // Light Sea Green
+    "#093472",
+    "#653712",
+    "#715261",
+    "#028393"
   ];
-  for (let i = 0; i < 28; i++) {
-    mainBox.innerHTML += `<div style = "width : 172px; height : 172px; background-color :${randomColors[i]}; border: 2px solid white";> <h3 style = "display:flex;justify-content:center;align-items:center;height:100%" onclick="copied('${randomColors[i]}')"> ${randomColors[i]} </p></div>`
+  for (let i = 0; i < 32; i++) {
+    mainBox.innerHTML += `<div style = "width : 150px; height : 150px; background-color :${randomColors[i]}; border: 2px solid white";> <h3 style = "display:flex;justify-content:center;align-items:center;height:100%" onclick="copied('${randomColors[i]}')"> ${randomColors[i]} </p></div>`
 
   }
 }
